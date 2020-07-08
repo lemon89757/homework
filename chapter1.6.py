78478ae70b7becf77371e454e0d3c4d4faddf3bc
Always-tt <tt20200220i@163.com>
2020-07-08 09:37:54 +0800

    练习

---
+++ chapter1.6.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding=utf-8
+name = input("what is your name?")
+age = input("how old are you?")
+
+print("your name is:",name)
+print("you are"+age+"years old")
+
+after_ten = int(age)+10
+print("your will be"+str(after_ten)+"yeras old after ten years.")
