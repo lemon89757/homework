3928733ac1ed1c69d4d41dd988fd1e023d839bc3
Always-tt <tt20200220i@163.com>
2020-07-08 09:01:48 +0800

    练习

---
+++ chapter1.1.py
@@ -0,0 +1,20 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 2
+2
+>>> 23
+23
+>>> 2+3
+5
+>>> id(2)
+1432287168
+>>> type(2)
+<class 'int'>
+>>> type(2.0)
+<class 'float'>
+>>> type(23333333333333333333333333333333333333333333333)
+<class 'int'>
+>>> x=2
+>>> x
+2
+>>> 
