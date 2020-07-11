b7dcf556d5d1b09062b5245c12958a56c4e2ab66
Always-tt <tt20200220i@163.com>
2020-07-12 00:46:44 +0800

    约瑟夫环（第一次）

---
+++ 约瑟夫环.py
@@ -0,0 +1,22 @@
+def joeseph_circle(a,b):
+    order = list(range(a))
+    while len(order) != 1:
+        x = b%a  
+        if x == 0:
+            order.pop(b-1)
+            list_1 = order[b:]
+            list_2 = order[:b]
+            order = list_1 + list_2
+            a = a-1
+        else:
+            order.pop(x-1)
+            list_3 = order[x:]
+            list_4 = order[:x]
+            order = list_3 + list_4
+            a = a-1
+    return order
+
+num_people = int(input("how many people:"))
+num_kill = int(input("the munber is:"))
+last_num = joeseph_circle(num_people,num_kill)
+print("the last one is:",last_num)
