0d9ea1d67e32bde2900d05c7ef58d9741e4a7b80
Always-tt <tt20200220i@163.com>
2020-07-15 12:30:27 +0800

    修改

--- 约瑟夫环.py
+++ 约瑟夫环.py
@@ -1,4 +1,6 @@
-def joeseph_circle(a,b):
+def joeseph_circle(num_person,num_killed):
+    a = num_person
+    b = num_killed
     assert isinstance(a,int)
     assert isinstance(b,int)
     assert a != 0
@@ -7,15 +9,15 @@ def joeseph_circle(a,b):
         x = b%a  
         if x == 0:
             order.pop(b-1)
-            list_1 = order[b:]
-            list_2 = order[:b]
-            order = list_1 + list_2
+            list_former = order[b:]
+            list_latter = order[:b]
+            order = list_former + list_latter
             a = a-1
         else:
             order.pop(x-1)
-            list_3 = order[x:]
-            list_4 = order[:x]
-            order = list_3 + list_4
+            list_former = order[x:]
+            list_latter = order[:x]
+            order = list_former + list_latter
             a = a-1
     assert len(order) == 1
     return order
